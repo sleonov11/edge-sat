c5c286d0380f7e5fff349be91ebc71d81a4e5c53
sleonov11 <leonoffstepan@gmail.com>
2026-07-08 13:31:19 +0300

    gaussian completed

---
+++ build/CMakeFiles/test_gaussian.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/test_gaussian.dir/link.d"
+  "CMakeFiles/test_gaussian.dir/tests/test_gaussian.cpp.o"
+  "CMakeFiles/test_gaussian.dir/tests/test_gaussian.cpp.o.d"
+  "test_gaussian"
+  "test_gaussian.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/test_gaussian.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
