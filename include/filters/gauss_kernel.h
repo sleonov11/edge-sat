37f359d45fd68c09d2775d63c6fb49edb57e4e8d
sleonov11 <leonoffstepan@gmail.com>
2026-08-02 10:45:41 +0300

    gauss ker

---
+++ include/filters/gauss_kernel.h
@@ -0,0 +1,19 @@
+#pragma once
+
+template <int RADIUS>
+struct GaussKernel {
+    static constexpr int SIZE = 2 * RADIUS + 1;
+    float k[SIZE];
+
+    constexpr GaussKernel(float sigma) : k{} {
+        float sum = 0;
+        for (int i = 0; i < SIZE; ++i) {
+            int d = i - RADIUS;
+            k[i] = std::exp(-(d*d) / (2.0f * sigma * sigma)) // c++20 constexpr
+            sum += k[i];
+        }
+        for (int i = 0; i < SIZE; ++i) k[i] /= sum;
+    }
+};
+
+inline constexpr GaussKernel<2> GAUSS_K2_15(1.5f);
